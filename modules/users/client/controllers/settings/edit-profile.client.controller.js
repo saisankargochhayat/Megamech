debb1969871140e76ff3aea655888ec87aea9499
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2016-04-15 18:30:18 +0530

    initial commit

---
+++ modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -0,0 +1,38 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('users')
+    .controller('EditProfileController', EditProfileController);
+
+  EditProfileController.$inject = ['$scope', '$http', '$location', 'Users', 'Authentication'];
+
+  function EditProfileController($scope, $http, $location, Users, Authentication) {
+    var vm = this;
+
+    vm.user = Authentication.user;
+    vm.updateUserProfile = updateUserProfile;
+
+    // Update a user profile
+    function updateUserProfile(isValid) {
+      vm.success = vm.error = null;
+
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.userForm');
+
+        return false;
+      }
+
+      var user = new Users(vm.user);
+
+      user.$update(function (response) {
+        $scope.$broadcast('show-errors-reset', 'vm.userForm');
+
+        vm.success = true;
+        Authentication.user = response;
+      }, function (response) {
+        vm.error = response.data.message;
+      });
+    }
+  }
+}());
